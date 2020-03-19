dc38415af2034da69e7841eeb11dd7c56db8b0d3
iampreethamveigas <veigasbros@gmail.com>
2020-03-19 23:39:09 +0530

    pool connections

--- src/ive-datamart-request-processer.js
+++ src/ive-datamart-request-processer.js
@@ -2,9 +2,22 @@ var AWS = require('aws-sdk');
 var glue = new AWS.Glue();
 var s3 = new AWS.S3();
 const lambda = new AWS.Lambda();
-
 const pg = require('pg')
 const { Pool } = require('pg');
+
+
+
+/*
+The pool configuration for pg connection
+*/
+const pool = new Pool({
+    host: process.env.db_host,
+    user: process.env.db_user,
+    max: process.env.db_max,
+    idleTimeoutMillis: process.env.db_idle_timeout,
+    connectionTimeoutMillis: process.env.db_connection_timeout,
+})
+
 
 var batchFlow = false;
 var business_entity;
